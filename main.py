c9c8352eeee987196e91a949b2f8412ce48afacf
Ivan Volkov <volkivanv@mail.ru>
2023-11-14 22:43:55 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# print('введите первое число')
+# a = float(input())
+# b = float(input('Введите второе число: '))
+# print(a, '+', b, ' = ', round(a * b,3))
+
+# i = 0
+# while i < 5:
+#     # if i == 3:
+#     #     break
+#     i = i + 1
+# else:
+#     print('Пожалуй')
+#     print('Хватит')
+# print(i)
+
+a = 'qwerty'
+for q in a:
+    print(q)
+
+line = ""
+for i in range(5):
+    line += "*"
+print(line)
