16ca5e93fcceb49f753d67c5eeb843ca37b88f25
Ivan Volkov <volkivanv@mail.ru>
2023-11-15 08:18:44 +0300

    ÐСделÐано дз

--- probe.py
+++ probe.py
@@ -1,1 +1,17 @@
+import math
+
 print("Something")
+coins = [0, 1, 0, 1, 1, 0, 1, 1, 0]
+
+# Введите ваше решение ниже
+
+
+print(min(coins.count(0), coins.count(1)))
+
+s = 12
+p = 27
+
+x1 = round((s - (s**2 - 4*p)**0.5)/2)
+x2 = round((s + (s**2 - 4*p)**0.5)/2)
+
+print(x1, x2)
